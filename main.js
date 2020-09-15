8cfcab66bd68742d4bf62dd9f981a8b821210222
Anti614 <julianikulina55@gmail.com>
2020-09-15 11:17:58 -0400

    created

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    let nav = $("#navbarContent");
+    $(".navbar a").click(function() {
+          
+        $("body,html").animate({
+         scrollTop:$("#" + $(this).data('value')).offset().top - $("#header").height()
+        }, 700);
+        nav.removeClass("show");
+
+    });
+    $('#carouselBanner').carousel({
+        interval: false
+    });
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=<12524098855>&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+});
